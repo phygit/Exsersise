ec1c0daf15009dbf79ac27ed9772d5260a8d5edc
phygit <mahami_n@yahoo.com>
2019-05-16 15:51:59 +0430

    2nd exersize

--- 2St.py
+++ 2St.py
@@ -1,3 +1,15 @@
-# hello my mother
-# add new line test
-mio mio avaz mishe!
+print("mio")
+print("test")
+
+print("i love my motehr")
+num = 98
+print(num)
+text="esteqlal"
+print(text)
+print("god pls help me")
+
+text = "test"
+print(text)
+
+
+
