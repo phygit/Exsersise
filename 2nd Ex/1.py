dadef1d1e097ebd64b66ca196b4583aa31618f54
phygit <mahami_n@yahoo.com>
2019-05-23 10:07:55 +0430

    2nd exersize if elif

---
+++ 2nd Ex/1.py
@@ -0,0 +1,22 @@
+
+
+
+num = 2
+print(num)
+
+text = " hello Abbas"
+print(text)
+text_1 = text.split()
+print(text_1)
+text_2 = text[1]
+print(text_2)
+text_3=text.split()
+print(text_3[1])
+text_4=text.replace("Abbas","my mother")
+print(text_4)
+num=20111983
+str=str(num)
+print(str)
+rep=num.replace("20","10")
+print(rep)
+
---
+++ 2nd Ex/odd.py
@@ -0,0 +1,5 @@
+num =101
+if num % 2 == 0:
+    print("even")
+else :
+    print("odd")
