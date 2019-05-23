03f0eaf695d22c557c77101d93b9f1c8c6ff48f5
phygit <mahami_n@yahoo.com>
2019-05-23 13:08:05 +0430

    2nd sesion ,1st exersize

--- 2nd Ex/1.py
+++ 2nd Ex/1.py
@@ -1,3 +1,24 @@
-wieght = 80
-height = 1.8
-bmi = weight / (height*height)
+format = input ("if you use kg/meter pls put YES if Pound/inc put No :")
+weight = float (input ("please enter your weight : "))
+height = float (input ("please enter your height : "))
+
+bmi = round((weight / (height * height)), 2)
+
+if format.lower() != "yes" :
+
+    bmi = bmi*703
+
+print(bmi)
+
+
+
+if bmi < 18.5 :
+    print("under weight")
+if bmi > 30 :
+    print("obesity")
+elif 18.5 < bmi < 25 :
+    print("Normal")
+else :
+    print("overweight")
+
+
