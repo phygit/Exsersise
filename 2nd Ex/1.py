117de43334b43ac514311d60720a560d2ba5e671
phygit <mahami_n@yahoo.com>
2019-05-23 11:04:39 +0430

    2nd exersize if elif remove juck files

--- 2nd Ex/1.py
+++ 2nd Ex/1.py
@@ -1,22 +1,3 @@
-
-
-
-num = 2
-print(num)
-
-text = " hello Abbas"
-print(text)
-text_1 = text.split()
-print(text_1)
-text_2 = text[1]
-print(text_2)
-text_3=text.split()
-print(text_3[1])
-text_4=text.replace("Abbas","my mother")
-print(text_4)
-num=20111983
-str=str(num)
-print(str)
-rep=num.replace("20","10")
-print(rep)
-
+wieght = 80
+height = 1.8
+bmi = weight / (height*height)
--- 2nd Ex/odd.py
+++
@@ -1,5 +0,0 @@
-num =101
-if num % 2 == 0:
-    print("even")
-else :
-    print("odd")
